151002d5a54c7141df7160d435e63e104125fe97
annasinitsyna <asinitsyna@yahoo.com>
2013-11-03 00:24:27 -0400

    fixed typo

--- donuts.py
+++ donuts.py
@@ -1,6 +1,6 @@
 daily_donuts = raw_input("Please enter the number of donuts per day: ")
 daily_donuts = int(daily_donuts)
-people_number = raw_input ("Please neter the number of people: ")
+people_number = raw_input ("Please enter the number of people: ")
 people_number = int(people_number)
 weekly_donuts = daily_donuts * 7
 print people_number,'customers will need to eat',weekly_donuts / people_number,'per week in order to finish them all'
