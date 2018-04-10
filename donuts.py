2dd69cf0af11bfbd0d861f98b72bf069fbec228b
Anna Sinitsyna <annasinitsyna@users.noreply.github.com>
2018-04-09 22:23:21 -0400

    Update donuts.py

--- donuts.py
+++ donuts.py
@@ -1,8 +1,8 @@
-daily_donuts = raw_input("Please enter the number of donuts per day: ")
+daily_donuts = raw_input("Please enter the number of donuts sold per day: ")
 daily_donuts = int(daily_donuts)
-people_number = raw_input ("Please enter the number of people: ")
+people_number = raw_input ("Please enter the number of customers at a donut shop: ")
 people_number = int(people_number)
 weekly_donuts = daily_donuts * 7
-print people_number,'customers will need to eat',weekly_donuts / people_number,'per week in order to finish them all'
+print people_number,'customers will need to eat',weekly_donuts / people_number,'donuts each week in order to finish them all'
 yearly_donuts = daily_donuts * 365
-print 'An average American will eat',yearly_donuts / people_number,'per year'
+print 'A typical customer will eat',yearly_donuts / people_number,'donuts per year'
